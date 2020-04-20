8a361269762b11173d74318976fa7c87475b1fc6
andreacw <andrea.worsoe@gmail.com>
2020-04-20 14:33:25 +0200

    adder så stoooore filer:)

---
+++ raspi_import.py
@@ -0,0 +1,43 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+from fft import *
+
+def raspi_import(path, channels=5):
+    """
+    Import data produced using adc_sampler.c.
+
+    Parameters
+    ----------
+    path: str
+        Path to file.
+    channels: int, optional
+        Number of channels in file.
+
+    Returns
+    -------
+    sample_period: float
+        Sample period
+    data: ndarray, uint16
+        Sampled data for each channel, in dimensions NUM_SAMPLES x NUM_CHANNELS.
+    """
+
+    with open(path, 'r') as fid:
+        sample_period = np.fromfile(fid, count=1, dtype=float)[0]
+        data = np.fromfile(fid, dtype=np.uint16)
+        data = data.reshape((-1, channels))
+    return sample_period, data
+
+samplePeriod, data = raspi_import('./ADCdata.bin')
+
+print(data)
+print(samplePeriod)
+
+data = np.array(data)
+
+#plt.plot(data[:, 4])
+
+
+#plt.show()
+
+fft_test(2000, data, 5)
